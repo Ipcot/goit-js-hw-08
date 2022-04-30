824ff518702d42c72256b59cfbb7c30febf8f271
Yuriy Pokhyl <woully@gmail.com>
2022-04-30 18:56:01 +0300

    task2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const CURRENT_TIME = "videoplayer-current-time";
+    // const iframe = document.querySelector('#vimeo-player');
+    const player = new Player(document.querySelector('#vimeo-player'));
+
+const saveTimeInStorage = (data) => {
+    localStorage.setItem(CURRENT_TIME, data.seconds)
+}
+const timeAfterPageReboot = localStorage.getItem(CURRENT_TIME)
+
+player.on('timeupdate', throttle(saveTimeInStorage, 1000));
+
+player.setCurrentTime(timeAfterPageReboot);
+
